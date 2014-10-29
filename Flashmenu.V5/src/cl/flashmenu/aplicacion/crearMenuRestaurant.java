232fe2a933b012c666a1020d08c38397db25a3aa
daniel18df <daniel18.df@gmail.com>
2014-10-29 01:37:21 -0300

    version 1.5

---
+++ Flashmenu.V5/src/cl/flashmenu/aplicacion/crearMenuRestaurant.java
@@ -0,0 +1,139 @@
+package cl.flashmenu.aplicacion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class crearMenuRestaurant extends Activity{
+	
+		private ProgressDialog pDialog;
+
+		JSONParser jsonParser = new JSONParser();
+		
+		EditText des;
+		EditText nom;
+		EditText precio;
+		
+		//private static String url_create_Rest = "http://190.153.212.77/daniel_fernandez/nuevoMenuRestaurant.php";
+		private static String url_create_Rest = "http://200.83.21.43/PHP/FlashmenuPHP/nuevoMenuRestaurant.php";
+
+		private static final String TAG_SUCCESS = "success";
+
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.crearmenurestaurant);
+
+			nom = (EditText) findViewById(R.id.nombreMenu);
+			des = (EditText) findViewById(R.id.descripcionMenu);
+			precio = (EditText) findViewById(R.id.precioMenu);
+			
+			
+			Button btnCreateRest = (Button) findViewById(R.id.crearmenu);
+
+			btnCreateRest.setOnClickListener(new View.OnClickListener() {
+
+				@Override
+				public void onClick(View view) {
+						new nuevoMenu().execute();
+				}
+			});
+		}
+
+		/**
+		 * Background Async Task to Create new Empleado
+		 * */
+		class nuevoMenu extends AsyncTask<String, String, String> {
+
+			/**
+			 * Before starting background thread Show Progress Dialog
+			 * */
+			@Override
+			protected void onPreExecute() {
+				super.onPreExecute();
+				pDialog = new ProgressDialog(crearMenuRestaurant.this);
+				pDialog.setMessage("Ingresando menu..");
+				pDialog.setIndeterminate(false);
+				pDialog.setCancelable(true);
+				pDialog.show();
+			}
+
+			/**
+			 * Creating Empleado
+			 * */
+			protected String doInBackground(String... args) {
+				
+			    String name = nom.getText().toString();
+				String desMenu = des.getText().toString();
+				String preMenu = precio.getText().toString();
+
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				
+				params.add(new BasicNameValuePair("Menu_nombre", name));
+				params.add(new BasicNameValuePair("Menu_descripcion", desMenu));
+				params.add(new BasicNameValuePair("Menu_precio", preMenu));
+
+				JSONObject json = jsonParser.makeHttpRequest(url_create_Rest,"POST", params);
+				
+				Log.d("Create Response", json.toString());
+
+				try {
+					int success = json.getInt(TAG_SUCCESS);
+
+					if (success == 1) {
+
+				Intent i = new Intent(getApplicationContext(), perfilRestaurant.class);
+						startActivity(i);
+			
+						//finish();
+					} else {
+						// fallo
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+
+				return null;
+			}
+
+			/**
+			 * After completing background task Dismiss the progress dialog
+			 * **/
+			protected void onPostExecute(String file_url) {
+				pDialog.dismiss();
+			}
+
+		}
+		
+		 @Override
+			public boolean onCreateOptionsMenu(Menu menu) {
+				getMenuInflater().inflate(R.menu.main, menu);
+				return true;
+			}
+
+			@Override
+			public boolean onOptionsItemSelected(MenuItem item) {
+				int id = item.getItemId();
+				if (id == R.id.action_settings) {
+					return true;
+				}
+				return super.onOptionsItemSelected(item);
+			}
+
+}
---
+++ Flashmenu.V5/src/cl/flashmenu/aplicacion/verMapa.java
@@ -0,0 +1,67 @@
+package cl.flashmenu.aplicacion;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class verMapa extends Activity{
+
+	private GoogleMap googleMap;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.vermapa);
+		
+		
+		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+		googleMap = mapFragment.getMap();
+		googleMap.setMyLocationEnabled(true);
+		
+		
+		googleMap.addMarker(new MarkerOptions().position(new LatLng(-33.013779, -71.543099)).title("MAIA").snippet("Vuelva a presionar el marker para mas informacion!"));
+		//googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+		
+		googleMap.setOnMarkerClickListener(new OnMarkerClickListener(){
+
+			@Override
+			public boolean onMarkerClick(Marker arg0) {
+				
+				
+				Toast.makeText(getApplicationContext(), arg0.getTitle(), Toast.LENGTH_LONG).show();
+				//////////
+				googleMap.setOnMarkerClickListener(new OnMarkerClickListener(){
+
+					@Override
+					public boolean onMarkerClick(Marker arg0) {
+						
+						Intent i = new Intent(getApplicationContext(), infoRestaurantes.class);
+						startActivity(i);
+
+						return false;
+					}
+				});
+				//////////
+				
+				return false;
+			}
+		});
+		
+	}
+		
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
